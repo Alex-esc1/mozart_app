88b2bd5e0088002c4b5de77da9da976de33eddd8
Alex <alexesc154@gmail.com>
2022-11-09 20:03:28 +0300

    refactoring

--- mozart_app/lib/screens/mozart_app_screen.dart
+++ mozart_app/lib/screens/mozart_app_screen.dart
@@ -10,28 +10,20 @@ class MozartAppScreen extends StatefulWidget {
 class _MozartAppScreenState extends State<MozartAppScreen> {
   final List<Item> items = [
     Item(
-      name: 'One',
-      imagePath: 'image/mozart-compose.jpg',
-      audioPath: 'audio/mozart.mp3',
+      name: "One",
+      imagePath: "image/mozart-compose.jpg",
+      audioPath: "audio/mozart.mp3",
     ),
     Item(
-      name: 'Two',
-      imagePath: 'image/mozart.jpg',
-      audioPath: 'audio/mozart_40.mp3',
+      name: "Two",
+      imagePath: "image/mozart.jpg",
+      audioPath: "audio/mozart_40.mp3",
     ),
   ];
 
   final AudioPlayer audioPlayer = AudioPlayer();
 
   int? playingIndex;
-
-  Widget showIcon(int currentIndex) {
-    if (playingIndex == currentIndex) {
-      return Icon(Icons.stop);
-    } else {
-      return Icon(Icons.play_arrow);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +35,7 @@ class _MozartAppScreenState extends State<MozartAppScreen> {
               return Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: Container(
-                  height: 200,
+                  height: 300,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
                     image: DecorationImage(
@@ -52,9 +44,19 @@ class _MozartAppScreenState extends State<MozartAppScreen> {
                     ),
                   ),
                   child: ListTile(
-                    title: Text(items[index].name),
+                    title: Text(
+                      items[index].name,
+                      style: const TextStyle(
+                        fontSize: 22.0,
+                        height: 2,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
                     leading: IconButton(
-                      icon: showIcon(index),
+                      iconSize: 50,
+                      icon: playingIndex == index
+                          ? const Icon(Icons.stop)
+                          : const Icon(Icons.play_arrow),
                       onPressed: (() {
                         if (playingIndex == index) {
                           audioPlayer.stop();
